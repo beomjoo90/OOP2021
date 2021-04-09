ccb2fb711624c4fd0f9352cdc68e6a1be3d0b819
beomjoo90 <beomjoo90@gmail.com>
2021-04-09 09:16:49 +0900

    2021년도 1학기 OOP 저장소 등록
    * 2021년도에 처음으로 등록한 폴더입니다.

---
+++ OOP2021_1/OOP2021_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string.h>
+
+int main()
+{
+	char data[100] { "hello world" };
+
+	printf("%s\n", data);
+
+	strcpy(data, "hello beomjoo seo");
+
+	printf("%s\n", data);
+
+	return 0;
+}

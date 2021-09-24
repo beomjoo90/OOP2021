1d334be42724e863cfa0b3c98dd50d6e6d8f867c
beomjoo90 <beomjoo90@gmail.com>
2021-09-24 14:52:53 +0900

    mouse, keyboard 입력 제어 로직 테스트
    * https://docs.microsoft.com/en-us/windows/console/reading-input-buffer-events?redirectedfrom=MSDN 에 나와 있는 코드를 본 프레임워크에 반영함
    * ReadConsoleInput 함수를 nonblocking으로 동작시키기 위해서 Get... 함수를 사용함

--- OOP2021_1/OOP2021_1.cpp
+++ OOP2021_1/OOP2021_1.cpp
@@ -8,7 +8,7 @@
 #include <cstdlib> // stdlib.h
 #include <string> // c++ string class
 #include <Windows.h>
-#include "Utils.h"
+#include "Utils.h"0
 
 // https://github.com/beomjoo90/OOP2021 , branch: 2학기
 
@@ -118,21 +118,96 @@ public:
 	GameObject** getGameObjects() { return gameObjects; }
 };
 
+static HANDLE hStdin;
+static DWORD fdwSaveOldMode;
+
+static char blankChars[80];
+
+static void ErrorExit(const char*);
+static void KeyEventProc(KEY_EVENT_RECORD);
+static void MouseEventProc(MOUSE_EVENT_RECORD);
+static void ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
+
 
 int main()
 {	
-	int major;
-	int minor;
+	DWORD cNumRead, fdwMode, i;
+	INPUT_RECORD irInBuf[128];
+	int counter = 0;
+	
+	
+	memset(blankChars, ' ', 80);
+	blankChars[79] = '\0';
+
+	// Get the standard inp
 
 	Screen  screen(20, 10);
 	Position pos{ 1, 2 };
 	char shape[] = "**    **     **";
 	Dimension sz{ (int)strlen(shape), 1 };
 
+
+	// Get the standard input handle.
+
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE)
+		ErrorExit("GetStdHandle");
+	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+		ErrorExit("GetConsoleMode");
+	fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+	if (!SetConsoleMode(hStdin, fdwMode))
+		ErrorExit("SetConsoleMode");
+
 	bool isLooping = true;
 	while (isLooping) {
+	
 		screen.clear();
 
+		if (GetNumberOfConsoleInputEvents(hStdin, &cNumRead)) {
+			Borland::gotoxy(0, 14);
+			printf("number of inputs %d\n", cNumRead);
+
+			if (cNumRead > 0) {
+
+				if (!ReadConsoleInput(
+					hStdin,      // input buffer handle
+					irInBuf,     // buffer to read into
+					128,         // size of read buffer
+					&cNumRead)) // number of records read
+					ErrorExit("ReadConsoleInput");
+				// Dispatch the events to the appropriate handler.
+
+				for (i = 0; i < cNumRead; i++)
+				{
+					switch (irInBuf[i].EventType)
+					{
+					case KEY_EVENT: // keyboard input
+						KeyEventProc(irInBuf[i].Event.KeyEvent);
+						break;
+
+					case MOUSE_EVENT: // mouse input
+						MouseEventProc(irInBuf[i].Event.MouseEvent);
+						break;
+
+					case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
+						ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+						break;
+
+					case FOCUS_EVENT:  // disregard focus events
+
+					case MENU_EVENT:   // disregard menu events
+						break;
+
+					default:
+						ErrorExit("Unknown event type");
+						break;
+					}
+				}
+			}
+			Borland::gotoxy(0, 0);
+		}
+
+		
 		screen.draw(pos, shape, sz);
 
 		screen.render();
@@ -143,5 +218,101 @@ int main()
 	}
 	printf("\nGame Over\n");
 
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
 	return 0;
+}
+
+void ErrorExit(const char* lpszMessage)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
+	
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
+	ExitProcess(0);
+}
+
+void KeyEventProc(KEY_EVENT_RECORD ker)
+{
+	Borland::gotoxy(0, 11);
+	printf("%s\r", blankChars);
+	switch (ker.wVirtualKeyCode) {
+	case VK_LBUTTON:
+		printf("left button ");
+		break;
+	case VK_BACK:
+		printf("back space");
+		break;
+	case VK_RETURN:
+		printf("enter key");
+		break;
+	case VK_LEFT:
+		printf("arrow left");
+		break;
+	case VK_UP:
+		printf("arrow up");
+		break;
+	default:
+		if (ker.wVirtualKeyCode >= 0x30 && ker.wVirtualKeyCode <= 0x39)
+			printf("Key event: %c ", ker.wVirtualKeyCode - 0x30 + '0');
+		else printf("Key event: %c ", ker.wVirtualKeyCode - 0x41 + 'A');
+		break;
+	}
+	
+	Borland::gotoxy(0, 0);
+}
+
+void MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+	Borland::gotoxy(0, 12);
+	printf("%s\r", blankChars);
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	printf("Mouse event: ");
+
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			printf("left button press %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+		{
+			printf("right button press \n");
+		}
+		else
+		{
+			printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		printf("mouse moved %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+		break;
+	case MOUSE_WHEELED:
+		printf("vertical mouse wheel\n");
+		break;
+	default:
+		printf("unknown\n");
+		break;
+	}
+	Borland::gotoxy(0, 0);
+}
+
+void ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+	Borland::gotoxy(0, 13);
+	printf("%s\r", blankChars);
+	printf("Resize event: ");
+	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
+	Borland::gotoxy(0, 0);
 }
